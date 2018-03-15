e7eab160cff9e03c6abb856b9589b07af95fc252
Reyes <reyes_palencia_romero@hotmail.com>
2018-03-15 18:00:55 +0100

    Segundo cambio con test

--- src/main/java/Ejercicio/PersonaFiscal.java
+++ src/main/java/Ejercicio/PersonaFiscal.java
@@ -1,5 +1,48 @@
 package Ejercicio;
 
 public class PersonaFiscal {
+	String nombre;
+	int Edad;
+	int Sueldo;
+	TipoImpuesto Impuesto;
 
+	public void setnNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setnEdad(int _Edad) {
+		this.Edad = _Edad;
+	}
+
+	public int getEdad() {
+		return this.Edad;
+	}
+
+	public void setSueldo(int _Sueldo) {
+		this.Edad = _Sueldo;
+	}
+
+	public int getSueldo() {
+		return this.Sueldo;
+	}
+
+	public int calcularImpuesto() {
+		return -1;
+	}
+
+	public void vincularTipoImpuesto(String tipo) {
+		if (tipo == "A") {
+			TipoImpuesto ImpuestoA = new TipoImpuesto(20, 30, "A");
+		} else if (tipo == "B") {
+			TipoImpuesto ImpuestoB = new TipoImpuesto(10, 5, "B");
+		} else if (tipo == "C") {
+			TipoImpuesto ImpuestoC = new TipoImpuesto(30, 25, "C");
+		} else {
+			TipoImpuesto ImpuestoD = new TipoImpuesto(10, 30, "D");
+		}
+	}
 }
--- src/main/java/Ejercicio/TipoImpuesto.java
+++ src/main/java/Ejercicio/TipoImpuesto.java
@@ -1,5 +1,39 @@
 package Ejercicio;
 
 public class TipoImpuesto {
+	int Porcentaje;
+	int Porcentaje30;
+	String Nombre;
+
+	public TipoImpuesto(int _porcentaje, int _porcentaje30, String _nombre) {
+		this.Porcentaje = _porcentaje;
+		this.Porcentaje30 = _porcentaje30;
+		this.Nombre = _nombre;
+
+	}
+
+	public void setPorcentaje(int _Porcentaje) {
+		this.Porcentaje = _Porcentaje;
+	}
+
+	public int getPorcentaje() {
+		return this.Porcentaje;
+	}
+
+	public void setPorcentaje30(int _Porcentaje30) {
+		this.Porcentaje30 = _Porcentaje30;
+	}
+
+	public int getPorcentaje30() {
+		return this.Porcentaje30;
+	}
+
+	public void setNombre(String _Nombre) {
+		this.Nombre = _Nombre;
+	}
+
+	public String getNombre() {
+		return this.Nombre;
+	}
 
 }
